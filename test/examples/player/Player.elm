dab25b983c3a0d205643ee508e9ccd021738e98a
John Watson <john.watson@gmx.co.uk>
2016-02-29 19:53:12 +0000

    Added expansion of repeats
    A repeat may be simple, or have variant endings.  These are first recognised within AbcPerformance and then expanded so that the performance contains both repeated sections.

--- test/examples/player/Player.elm
+++ test/examples/player/Player.elm
@@ -2,28 +2,31 @@ module Player where
 
 import Effects exposing (Effects, task)
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, on, targetChecked)
+import Html.Attributes exposing (type', checked)
 import Http exposing (..)
 import Task exposing (..)
 import List exposing (..)
 import Maybe exposing (..)
 import String exposing (..)
 import Result exposing (Result, formatError)
+import Signal exposing (Address)
 import Performance exposing (..)
 import AbcPerformance exposing (..)
-import Repeats exposing (Repeats, Section)
+import Repeats exposing (Repeats, Section, buildRepeatedMelody)
 import Abc.ParseTree exposing (..)
 import Abc exposing (parse, parseError)
 
 -- MODEL
 
 type alias Model =
-    { performance : Result String (MelodyLine, Repeats)
+    { performance : Result String (MelodyLine, Repeats) 
+    , expandRepeats : Bool
     }
 
 init : String -> (Model, Effects Action)
 init topic =
-  ( { performance = Err "not started"  }
+  ( { performance = Err "not started", expandRepeats = False  }
   , Effects.none
   )
 
@@ -31,6 +34,7 @@ init topic =
 
 type Action
     = NoOp
+    | ExpandRepeats Bool
     | Load String
     | Abc (Result String (MelodyLine, Repeats) )
 
@@ -39,9 +43,11 @@ update action model =
   case action of
     NoOp -> (model, Effects.none )
 
-    Abc result ->  ( { performance = result }, Effects.none ) 
+    ExpandRepeats b -> ( { model | expandRepeats = b}, Effects.none )
 
-    Load url -> (model, loadAbc url) 
+    Abc result ->  ( { model | performance = result }, Effects.none ) 
+
+    Load url -> (model, loadAbc url model.expandRepeats) 
 
    
 mToList : Maybe (List a) -> List a
@@ -52,8 +58,8 @@ mToList m = case m of
 
 
 {- load an ABC file -}
-loadAbc : String -> Effects Action
-loadAbc url = 
+loadAbc : String -> Bool -> Effects Action
+loadAbc url expandRepeats = 
       let settings =  { defaultSettings | desiredResponseType  = Just "text/plain; charset=utf-8" }   
         in
           Http.send settings
@@ -64,7 +70,7 @@ loadAbc url =
                           } 
           |> Task.toResult
           |> Task.map extractResponse
-          |> Task.map parseLoadedFile
+          |> Task.map (parseLoadedFile expandRepeats)
           |> Task.map Abc
           |> Effects.task
 
@@ -86,18 +92,20 @@ extractResponse result =
 toInt : String -> Int
 toInt = String.toInt >> Result.toMaybe >> Maybe.withDefault 0
 
-toPerformance : Result String AbcTune -> Result String (MelodyLine, Repeats)
-toPerformance r = Result.map fromAbc r
+toPerformance : Bool -> Result String AbcTune -> Result String (MelodyLine, Repeats)
+toPerformance expandRepeats r = 
+   Result.map (melodyFromAbc expandRepeats) r
 
-parseLoadedFile : Result String Value -> Result String (MelodyLine, Repeats)
-parseLoadedFile r = 
+
+parseLoadedFile : Bool -> Result String Value -> Result String (MelodyLine, Repeats)
+parseLoadedFile expandRepeats r = 
   case r of
     Ok text -> case text of
       Text s -> 
         s 
          |> parse
          |> formatError parseError 
-         |> toPerformance
+         |> toPerformance expandRepeats
       Blob b -> 
         Err "Blob unsupported"
     Err e -> Err e
@@ -121,8 +129,23 @@ view address model =
     , button [ onClick address (Load "abc/twobars.abc") ] [ text "two bars" ]
     , button [ onClick address (Load "abc/baraccidental.abc") ] [ text "bar accidental" ]
     , button [ onClick address (Load "abc/tie.abc") ] [ text "tie" ]
+    , button [ onClick address (Load "abc/variantending.abc") ] [ text "variant ending" ]
+    , div [  ] (checkbox address model.expandRepeats ExpandRepeats "expand repeats")
     , div [  ] [ text ("parse result: " ++ (viewPerformanceResult model.performance)) ]
     ]
+
+checkbox : Address Action -> Bool -> (Bool -> Action) -> String -> List Html
+checkbox address isChecked tag name =
+  [ input
+      [ type' "checkbox"
+      , checked isChecked
+      , on "change" targetChecked (Signal.message address << tag)
+      ]
+      []
+  , text name
+  , br [] []
+  ]
+
 
 -- INPUTS
 
